0bce68e7daa5efe2ac92fddac05c5484faab57d4
Dylan Pilsner <dpilsner@cetrogar.com.ar>
2022-10-19 16:55:32 -0300

    end-point auth finalizado

--- client/state.ts
+++ client/state.ts
@@ -1,3 +1,29 @@
 export { state };
 
-const state = 1;
+const state = {
+  data: {
+    gameState: {
+      playerId: "",
+      choice: "",
+      name: "",
+      online: false,
+      start: false,
+      score: 0,
+    },
+  },
+
+  getState() {
+    return this.data;
+  },
+
+  setState(newState) {
+    const currentState = this.getState();
+    currentState.gameState = newState;
+  },
+
+  pushName(name) {
+    const currentGameState = this.getState().gameState;
+    currentGameState.name = name;
+    this.setState(currentGameState);
+  },
+};
--- server/index.ts
+++ server/index.ts
@@ -12,8 +12,9 @@ const userCollection = firestore.collection("users");
 const roomCollection = firestore.collection("rooms");
 
 app.post("/new-room", (req, res) => {
+  const { gameState } = req.body;
   const roomRef = rtdb.ref("rooms/" + nanoid());
-  roomRef.set({ playerId: "test" }).then(() => {
+  roomRef.set(gameState).then(() => {
     const roomLongId = roomRef.key;
     const roomId = 1000 + Math.floor(Math.random() * 999);
     roomCollection
@@ -22,9 +23,26 @@ app.post("/new-room", (req, res) => {
       .then(() => {
         res.json({
           id: roomId.toString(),
+          longId: roomLongId,
         });
       });
   });
+});
+
+app.post("/auth", (req, res) => {
+  const { name } = req.body;
+  userCollection
+    .where("name", "==", name)
+    .get()
+    .then((searchResponse) => {
+      if (searchResponse.empty) {
+        userCollection.add({ name }).then((newUserRef) => {
+          res.json({ id: newUserRef.id });
+        });
+      } else {
+        res.json({ id: searchResponse.docs[0].id });
+      }
+    });
 });
 
 // app.get("/test", (req, res) => {
