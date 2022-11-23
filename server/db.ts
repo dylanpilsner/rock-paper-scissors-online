fc8a66baf42a76a093696d498c224b1b10cd9cb6
Dylan Pilsner <dpilsner@cetrogar.com.ar>
2022-11-23 12:17:16 -0300

    test

--- server/db.ts
+++ server/db.ts
@@ -1,11 +1,15 @@
 import * as admin from "firebase-admin";
 import { applicationDefault } from "firebase-admin/app";
-const serviceAccount = require("./key.json");
+// const serviceAccount = require("./key.json");
 
 admin.initializeApp({
   credential: applicationDefault(),
   databaseURL: "https://apx-dwf-m6-23482-default-rtdb.firebaseio.com",
 });
+// admin.initializeApp({
+//   credential: serviceAccount as any,
+//   databaseURL: "https://apx-dwf-m6-23482-default-rtdb.firebaseio.com",
+// });
 
 const firestore = admin.firestore();
 const rtdb = admin.database();
